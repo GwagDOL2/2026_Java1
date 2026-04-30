7a96cbc61463eee3c99cf414e3820dc295529c0d
GWAGDOL <a77676652@gmail.com>
2026-04-30 11:44:08 +0900

    ^^

---
+++ src/ai0430/ForTest1.java
@@ -0,0 +1,11 @@
+package ai0430;
+
+public class ForTest1 {
+    public static void main(String[] args) {
+        String[] subjecArr = {"자바", "데베", "융합UI", "인공개"};
+        for (int i = 0; i < 4; i++) {
+            System.out.println("저는 " + subjecArr[i] + " 언어를 이번에 처음 공부해요.");
+
+        }
+    }
+}
